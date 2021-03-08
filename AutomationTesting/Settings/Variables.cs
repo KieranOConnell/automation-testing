6dd34553f0fc958e95db8ef2379ea3a3c5e6b51f
Kieran O'Connell <kieran.oconnell@students.ittralee.ie>
2021-03-08 02:40:29 +0000

    Created Variables class to store information needed in test cases

---
+++ AutomationTesting/Settings/Variables.cs
@@ -0,0 +1,10 @@
+﻿namespace AutomationTesting.Settings
+{
+    public class Variables
+    {
+        public const string Website = "http://automationpractice.com/index.php";
+        public const string Email = "koctestingacc@gmail.com";
+        public const string Password = "";
+        public const string User = "Kieran O'Connell";
+    }
+}
