7c3117c77a68cf89bb09f1dbce3e82781b9c3fba
Kieran O'Connell <kieran.oconnell@students.ittralee.ie>
2021-03-08 02:36:31 +0000

    Created Browser class to be store helper methods for the browser

---
+++ AutomationTesting/Utilities/Browser.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace AutomationTesting.Utilities
+{
+    public class Browser
+    {
+        public static void WaitForElement(IWebDriver driver, IWebElement element)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(element));
+        }
+
+        public static void RemoveAttribute(IWebDriver driver, IWebElement element, string attribute)
+        {
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("arguments[0].removeAttribute(arguments[1])", element, attribute);
+        }
+    }
+}
