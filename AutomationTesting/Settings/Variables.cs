8517ff35e0956f139038f3bd89c73435ed6642cc
Kieran O'Connell <kieran.oconnell@students.ittralee.ie>
2021-03-09 17:00:31 +0000

    Improved readability of Variables class and added more information

--- AutomationTesting/Settings/Variables.cs
+++ AutomationTesting/Settings/Variables.cs
@@ -2,10 +2,13 @@
 {
     public class Variables
     {
+        // General
         public const string Website = "http://automationpractice.com/index.php";
         public const string Email = "koctestingacc@gmail.com";
         public const string Password = "";
         public const string User = "Kieran O'Connell";
+
+        // CreateUser test
         public const string FirstName = "Test";
         public const string LastName = "Ing";
         public const string BirthDay = "1";
@@ -17,5 +20,10 @@
         public const string Country = "United States";
         public const string Zip = "10001";
         public const string PhoneNumber = "0861234567";
+
+        // ContactUs test
+        public const string SubjectHeading = "Customer service";
+        public const string Message = "Test message";
+        public const string SuccessfulMessage = "Your message has been successfully sent to our team.";
     }
 }
