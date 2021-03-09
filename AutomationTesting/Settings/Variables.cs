216c0c58cc788fbc12eeb815143d3df87e743919
Kieran O'Connell <kieran.oconnell@students.ittralee.ie>
2021-03-09 14:38:15 +0000

    Updated Variables class to store information for future test cases

--- AutomationTesting/Settings/Variables.cs
+++ AutomationTesting/Settings/Variables.cs
@@ -6,5 +6,16 @@
         public const string Email = "koctestingacc@gmail.com";
         public const string Password = "";
         public const string User = "Kieran O'Connell";
+        public const string FirstName = "Test";
+        public const string LastName = "Ing";
+        public const string BirthDay = "1";
+        public const string BirthMonth = "5";
+        public const string BirthYear = "1999";
+        public const string Address = "Test Address";
+        public const string City = "New York";
+        public const string State = "New York";
+        public const string Country = "United States";
+        public const string Zip = "10001";
+        public const string PhoneNumber = "0861234567";
     }
 }
