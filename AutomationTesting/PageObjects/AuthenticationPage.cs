668bbe361628671bbcce7e327d8338dbbee4a26d
Kieran O'Connell <kieran.oconnell@students.ittralee.ie>
2021-03-09 14:37:06 +0000

    Created new page object CreateAccountPage for test cases

--- AutomationTesting/PageObjects/AuthenticationPage.cs
+++ AutomationTesting/PageObjects/AuthenticationPage.cs
@@ -9,10 +9,14 @@ namespace AutomationTesting.PageObjects
         #region Page Elements
         [FindsBy(How = How.Id, Using = "email")]
         public IWebElement txtEmail = null;
+        [FindsBy(How = How.Id, Using = "email_create")]
+        public IWebElement txtEmailCreate = null;
         [FindsBy(How = How.Id, Using = "passwd")]
         public IWebElement txtPassword = null;
         [FindsBy(How = How.Id, Using = "SubmitLogin")]
         public IWebElement btnLogin = null;
+        [FindsBy(How = How.Id, Using = "SubmitCreate")]
+        public IWebElement btnCreate = null;
         #endregion
 
         public AuthenticationPage(IWebDriver driver)
---
+++ AutomationTesting/PageObjects/CreateAccountPage.cs
@@ -0,0 +1,46 @@
+﻿using AutomationTesting.Base;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace AutomationTesting.PageObjects
+{
+    public class CreateAccountPage : TestBase
+    {
+        #region Page Elements
+        [FindsBy(How = How.Id, Using = "id_gender1")]
+        public IWebElement rbnGender = null;
+        [FindsBy(How = How.Id, Using = "customer_firstname")]
+        public IWebElement txtFirstName = null;
+        [FindsBy(How = How.Id, Using = "customer_lastname")]
+        public IWebElement txtLastName = null;
+        [FindsBy(How = How.Id, Using = "passwd")]
+        public IWebElement txtPassword = null;
+        [FindsBy(How = How.Id, Using = "days")]
+        public IWebElement ddlDays = null;
+        [FindsBy(How = How.Id, Using = "months")]
+        public IWebElement ddlMonths = null;
+        [FindsBy(How = How.Id, Using = "years")]
+        public IWebElement ddlYears = null;
+        [FindsBy(How = How.Id, Using = "address1")]
+        public IWebElement txtAddress = null;
+        [FindsBy(How = How.Id, Using = "city")]
+        public IWebElement txtCity = null;
+        [FindsBy(How = How.Id, Using = "id_state")]
+        public IWebElement ddlState = null;
+        [FindsBy(How = How.Id, Using = "postcode")]
+        public IWebElement txtZip = null;
+        [FindsBy(How = How.Id, Using = "id_country")]
+        public IWebElement ddlCountry = null;
+        [FindsBy(How = How.Id, Using = "phone_mobile")]
+        public IWebElement txtPhoneNumber = null;
+        [FindsBy(How = How.Id, Using = "submitAccount")]
+        public IWebElement btnRegister = null;
+        #endregion
+
+        public CreateAccountPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+    }
+}
