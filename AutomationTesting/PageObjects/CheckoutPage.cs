4af4fe057e042b5b6ff444584cba67c2d6b5b345
Kieran O'Connell <kieran.oconnell@students.ittralee.ie>
2021-03-11 13:18:49 +0000

    Created CheckoutPage and ProductPage page objects

---
+++ AutomationTesting/PageObjects/CheckoutPage.cs
@@ -0,0 +1,32 @@
+﻿using AutomationTesting.Base;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace AutomationTesting.PageObjects
+{
+    public class CheckoutPage : TestBase
+    {
+        #region Page Elements
+        [FindsBy(How = How.XPath, Using = "//td/p[@class='product-name']/a")]
+        public IWebElement lblProduct = null;
+        [FindsBy(How = How.XPath, Using = "//p/a/span[contains(text(), 'Proceed to checkout')]")]
+        public IWebElement btnSummaryCheckout = null;
+        [FindsBy(How = How.XPath, Using = "//button/span[contains(text(), 'Proceed to checkout')]")]
+        public IWebElement btnAddressCheckout = null;
+        [FindsBy(How = How.Id, Using = "cgv")]
+        public IWebElement chkTermsOfService = null;
+        [FindsBy(How = How.XPath, Using = "//a[@title='Pay by check.']")]
+        public IWebElement lnkPayByCheck = null;
+        [FindsBy(How = How.XPath, Using = "//span[contains(text(), 'I confirm my order')]")]
+        public IWebElement btnConfirmOrder = null;
+        [FindsBy(How = How.XPath, Using = "//p[contains(@class, 'alert')]")]
+        public IWebElement lblResponse = null;
+        #endregion
+
+        public CheckoutPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+    }
+}
---
+++ AutomationTesting/PageObjects/ProductPage.cs
@@ -0,0 +1,28 @@
+﻿using AutomationTesting.Base;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace AutomationTesting.PageObjects
+{
+    public class ProductPage : TestBase
+    {
+        #region Page Elements
+        [FindsBy(How = How.Id, Using = "quantity_wanted")]
+        public IWebElement txtQuantity = null;
+        [FindsBy(How = How.Id, Using = "group_1")]
+        public IWebElement ddlSize = null;
+        [FindsBy(How = How.XPath, Using = "//span[contains(text(), 'Add to cart')]")]
+        public IWebElement btnAddToCart = null;
+        [FindsBy(How = How.XPath, Using = "//div[contains(@class, 'cart_product')]/h2")]
+        public IWebElement lblResponse = null;
+        [FindsBy(How = How.XPath, Using = "//span[contains(text(), 'Proceed to checkout')]")]
+        public IWebElement btnProceedToCheckout = null;
+        #endregion
+
+        public ProductPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+    }
+}
