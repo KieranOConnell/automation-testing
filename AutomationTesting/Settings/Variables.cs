1f7873161c0db4c12283fbbf0b9229772806820e
Kieran O'Connell <kieran.oconnell@students.ittralee.ie>
2021-04-27 01:50:08 +0100

    Resolved security issue with package version

--- AutomationTesting/Settings/Variables.cs
+++ AutomationTesting/Settings/Variables.cs
@@ -5,7 +5,7 @@
         // General
         public const string Website = "http://automationpractice.com/index.php";
         public const string Email = "koctestingacc@gmail.com";
-        public const string Password = "";
+        public const string Password = "testing123";
         public const string User = "Kieran O'Connell";
 
         // CreateUser test
