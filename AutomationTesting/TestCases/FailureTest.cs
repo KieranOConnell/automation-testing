f00df2d021764891ebda2edba52ecf0dcfde9c2f
Kieran O'Connell <kieran.oconnell@students.ittralee.ie>
2021-04-27 01:11:46 +0100

    Added FailureTest and SkipTest to show logging with KlovReporter

---
+++ AutomationTesting/TestCases/FailureTest.cs
@@ -0,0 +1,36 @@
+﻿using AutomationTesting.Base;
+using AventStack.ExtentReports;
+using NUnit.Framework;
+using System;
+
+namespace AutomationTesting.TestCases
+{
+    [TestFixture]
+    [FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+    [Parallelizable(ParallelScope.Children)]
+    public class FailureTest : TestBase
+    {
+        [Test]
+        [TestCaseSource(typeof(TestBase), "TestData")]
+        [Category("Failure")]
+        [Description("Test that will fail on purpose")]
+        public void Failure(string browserName, string operatingSystem)
+        {
+            try
+            {
+                #region Setting up browser and OS for testing
+                test = report.CreateTest(String.Format("FailureTest - {0}, {1}", browserName, operatingSystem));
+                test.Log(Status.Info, "Starting the FailureTest");
+
+                SetUp(browserName, operatingSystem);
+
+                throw new Exception("FailureTest has failed");
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                test.Log(Status.Fail, ex.Message);
+            }
+        }
+    }
+}
---
+++ AutomationTesting/TestCases/SkipTest.cs
@@ -0,0 +1,34 @@
+﻿using AutomationTesting.Base;
+using AventStack.ExtentReports;
+using NUnit.Framework;
+using System;
+
+namespace AutomationTesting.TestCases
+{
+    [TestFixture]
+    [FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+    [Parallelizable(ParallelScope.Children)]
+    public class SkipTest : TestBase
+    {
+        [Test]
+        [TestCaseSource(typeof(TestBase), "TestData")]
+        [Category("Skip")]
+        [Description("Test to demonstrate the skip feature")]
+        public void Skip(string browserName, string operatingSystem)
+        {
+            try
+            {
+                #region Setting up browser and OS for testing
+                test = report.CreateTest(String.Format("SkipTest - {0}, {1}", browserName, operatingSystem));
+                test.Log(Status.Skip, "SkipTest has been skipped");
+
+                SetUp(browserName, operatingSystem);
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                test.Log(Status.Fail, ex.Message);
+            }
+        }
+    }
+}
